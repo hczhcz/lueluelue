c526de9d21ef17ff5849879308ffdaf20833c046
hcz <h.cz@qq.com>
2014-06-04 00:31:00 +0800

    init

---
+++ player.js
@@ -0,0 +1,62 @@
+var maxLeft = 10;
+var maxRight = 20;
+
+function playExec(data, step) {
+    if (step >= data.length) return;
+
+    var lText = '';
+    var mText = '';
+    var rText = '';
+
+    for (var i = Math.max(0, step - maxLeft); i < step; ++i) {
+        lText += data[i][0];
+    }
+
+    mText = data[step][0];
+
+    for (var i = step + 1; i < Math.min(data.length, step + maxRight); ++i) {
+        rText += data[i][0];
+    }
+
+    mText += rText.substring(0, data[step][1]);
+    rText = rText.substring(data[step][1]);
+
+    data_left.innerHTML = '';
+    data_main.innerHTML = '';
+    data_right.innerHTML = '';
+
+    data_left.appendChild(document.createTextNode(lText));
+    data_main.appendChild(document.createTextNode(mText));
+    data_right.appendChild(document.createTextNode(rText));
+}
+
+var playing = false;
+var playingData = null;
+var playingStep = 0;
+var playingTick = 100;
+
+function playTimer() {
+    if (playing) {
+        playExec(playingData, playingStep);
+        playingStep++;
+    }
+
+    setTimeout(playTimer, playingTick);
+}
+
+function playLoad(data, step) {
+    playingData = data;
+    playingStep = step;
+}
+
+function playStart() {
+    playing = true;
+}
+
+function playPause() {
+    playing = false;
+}
+
+playLoad(parse(''), 0);
+playTimer();
+playStart();
---
+++ textctrl.js
@@ -0,0 +1,28 @@
+/*
+    Lue~Lue~Lue~ Reading Accelerator
+    By HCZ
+*/
+
+function exampleData() {
+    // [data, extra_highlighted]
+    return [
+        ['今天', 0],
+        ['我', 2],
+        ['没有', 0],
+        ['吃药', 1],
+        ['，', 0],
+        ['感觉', 0],
+        ['萌', 2],
+        ['萌', 1],
+        ['哒', 1],
+        ['！', 0]
+    ]
+}
+
+function parse_1(data) {
+    //
+}
+
+function parse(data) {
+    return exampleData();
+}
