910c0cf1b0d7471b52cc4f9cb53ad0239a033c0d
hcz <h.cz@qq.com>
2014-06-05 13:45:04 +0800

    working on tick controlling

--- player.js
+++ player.js
@@ -1,47 +1,65 @@
 var maxLeft = 10;
 var maxRight = 20;
-var initStep = -10;
+var initStep = -5;
 
 var playing = false;
 var playingData = [];
 var playingStep = initStep;
 var playingTick = 100;
 
-function playExec(data, step) {
-    if (step >= data.length) return false;
-    if (step < 0) step = 0;
+function getMainWidth() {
+    var result = data_main.clientWidth / data_main.clientHeight;
+    alert(result);
+    return result && result > 0.5 ? result : 0.5;
+}
+
+function playExec(data, step) { // Return next tick rate
+    if (step >= data.length) return 0;
 
     var lText = '';
     var mText = '';
+    var xText = '';
     var rText = '';
 
-    for (var i = Math.max(0, step - maxLeft); i < step; ++i) {
+    for (var i = Math.max(0, step - maxLeft); i < Math.min(data.length, step); ++i) {
         lText += data[i][0];
     }
 
-    mText = data[step][0];
+    if (step >= 0) {
+        mText = data[step][0];
+    } else {
+        for (var i = step; i < 0; ++i) mText += '.';
+    }
 
-    for (var i = step + 1; i < Math.min(data.length, step + maxRight); ++i) {
+    for (var i = Math.max(0, step + 1); i < Math.min(data.length, step + maxRight); ++i) {
         rText += data[i][0];
     }
 
-    mText += rText.substring(0, data[step][1]);
-    rText = rText.substring(data[step][1]);
+    if (step >= 0) {
+        xText += rText.substring(0, data[step][1]);
+        rText = rText.substring(data[step][1]);
+    }
 
     data_left.innerHTML = '';
     data_main.innerHTML = '';
+    data_main_extra.innerHTML = '';
     data_right.innerHTML = '';
 
     data_left.appendChild(document.createTextNode(lText));
     data_main.appendChild(document.createTextNode(mText));
+    data_main_extra.appendChild(document.createTextNode(xText));
     data_right.appendChild(document.createTextNode(rText));
 
-    return true;
+    return getMainWidth();
 }
 
 function playTimer() {
+    var tickRate = 1;
+
     if (playing) {
-        if (playExec(playingData, playingStep)) {
+        tickRate = playExec(playingData, playingStep);
+
+        if (tickRate) {
             playingStep++;
         } else {
             playing = false;
@@ -49,13 +67,13 @@ function playTimer() {
         }
     }
 
-    setTimeout(playTimer, playingTick);
+    setTimeout(playTimer, tickRate * playingTick);
 }
 
 function playLoad(data, step, init) {
     playingData = data;
-    playingStep = step;
-    playExec(playingData, init ? initStep : playingStep);
+    playingStep = init ? initStep : step;
+    playExec(playingData, playingStep);
 }
 
 function playStartPause() {
